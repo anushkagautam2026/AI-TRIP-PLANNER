e9f519e04159323b1bcd4b15e1bef3811cb7b2a4
ANUSHKA <chinkanu2020@gmail.com>
2025-03-21 23:24:17 +0530

    set up

---
+++ src/Create-trip/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+function CreateTrip() {
+  return (
+  <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
+    <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+    <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a
+    customized itinerary based on your preferences.</p>
+    <div>
+      <div className='mt-20'>
+      <h2 className='text-xl font-medium my-3'>What is your destination?</h2>
+      
+      </div>
+    </div>
+  </div>
+  
+  )
+}
+export default CreateTrip
